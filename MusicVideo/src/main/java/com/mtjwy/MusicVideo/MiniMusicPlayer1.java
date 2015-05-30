07506e5dbe4cc3c6c494f674988a8ac9fda5e64f
Yu Wu <wuyucsu@gmail.com>
2015-05-30 18:03:56 -0500

    version 3: drawing graphics in time with the music

--- MusicVideo/src/main/java/com/mtjwy/MusicVideo/MiniMusicPlayer1.java
+++ MusicVideo/src/main/java/com/mtjwy/MusicVideo/MiniMusicPlayer1.java
@@ -1,8 +1,52 @@
 package com.mtjwy.MusicVideo;
 
-import javax.sound.midi.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
-public class MiniMusicPlayer1 implements ControllerEventListener{
+import javax.sound.midi.ControllerEventListener;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MiniMusicPlayer1 {
+	
+	static JFrame f = new JFrame("My First Music Video");
+	static MyDrawPanel m1;
+	
+	class MyDrawPanel extends JPanel implements ControllerEventListener {
+		boolean msg = false;
+		
+		public void controlChange(ShortMessage event) {
+			msg = true;
+			repaint();
+		}
+		
+		public void paintComponent(Graphics g) {
+			if (msg) {
+				Graphics2D g2 = (Graphics2D) g;
+				
+				//generate a random color
+				int r = (int) (Math.random() * 250);
+				int gr = (int) (Math.random() * 250);
+				int b = (int) (Math.random() * 250);
+				g.setColor(new Color(r, gr, b));
+				
+				int ht = (int) ((Math.random() * 120) + 10);
+				int width = (int) ((Math.random() * 120) + 10);
+				int x = (int) ((Math.random() * 40) + 10);
+				int y = (int) ((Math.random() * 40) + 10);
+				g.fillRect(x, y, ht, width);
+				msg = false;
+			}
+		}
+		
+	}
 	public static MidiEvent makeEvent (int comd, int chan, int one, int two, int tick) {
 		MidiEvent event = null;
 		try {
@@ -15,38 +59,43 @@ public class MiniMusicPlayer1 implements ControllerEventListener{
 		return event;
 	}
 	
+	public void setUpGui() {
+		m1 = new MyDrawPanel();
+		f.setContentPane(m1);
+		f.setBounds(30,30, 300, 300);
+		f.setVisible(true);
+	}
 	
 	public void go() {
+		setUpGui();
 		try {
 			Sequencer sequencer = MidiSystem.getSequencer();
 			sequencer.open();
 			
 			int[] eventsIWant = {127};
-			sequencer.addControllerEventListener(this, eventsIWant);
+			sequencer.addControllerEventListener(m1, eventsIWant);
 			
 			Sequence seq = new Sequence(Sequence.PPQ, 4);
 			Track track = seq.createTrack();
 			
-			//NOTEON and NOTEOFF pairs, from piano note 5 to piano note 61
+			//NOTEON and NOTEOFF pairs, random
+			int r = 0;
 			for (int i = 5; i < 61; i += 4) {
-				track.add(makeEvent(144, 1, i, 100, i));
+				r = (int) ((Math.random() * 50) + 1);
+				track.add(makeEvent(144, 1, r, 100, i));
 				track.add(makeEvent(176, 1, 127, 0, i));
-				track.add(makeEvent(128, 1, i, 100, i + 2));
+				track.add(makeEvent(128, 1, r, 100, i + 2));
 			}
 			
 			//start running
 			sequencer.setSequence(seq);
-			sequencer.setTempoInBPM(220);
+			sequencer.setTempoInBPM(120);
 			sequencer.start();
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
 	
-	public void controlChange(ShortMessage event) {
-		System.out.println("la");
-		
-	}
 	
 	public static void main(String[] args) {
 		MiniMusicPlayer1 mini = new MiniMusicPlayer1();
